dfb3cfbf938e7965a96b3b2dec3a4d481a867d15
s.richard <simonrichard9@gmail.com>
2021-06-28 22:51:30 +0200

    add healthcheck controller for gcp test

--- src/main/java/org/holiday/api/HealthCheckController.java
+++ src/main/java/org/holiday/api/HealthCheckController.java
@@ -6,12 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping(path = "/api/health", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/api/health")
 public class HealthCheckController {
 
     @GetMapping
     public ResponseEntity<?> healthCheck() {
-        return ResponseEntity.ok("Holiday service stauts: green ;)");
+        return ResponseEntity.ok(Map.of(
+                "service", "holiday-service",
+                "status", "green (I say green !)",
+                "verions", "v1.0.0"
+                ));
     }
 }
